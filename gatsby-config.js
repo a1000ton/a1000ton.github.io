9d5ede9772bdbf6075ae4ee7f04de1fbbbcca72c
Amilton Vanderlei Baracy Junior <laamilton.a@MacBook-Pro-de-Amilton.local>
2020-09-30 20:04:51 -0300

    Remove comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Amilton Baracy`,
+    description: `A blog about development and games stuff, personal things and random posts`,
+    author: `@a1000ton`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,11 +24,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/sapo_sabio_transp.svg`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
