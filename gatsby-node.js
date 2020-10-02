4bf18cc972cb8401b4287176f1fea1b44377ff92
Amilton Vanderlei Baracy Junior <laamilton.a@MacBook-Pro-de-Amilton.local>
2020-10-01 22:07:08 -0300

    Creation of posts with markdown

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,34 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+  const postTemplate = require.resolve(`./src/templates/postTemplate.js`)
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.slug,
+      component: postTemplate,
+      context: {
+        slug: node.frontmatter.slug,
+      },
+    })
+  })
+}
