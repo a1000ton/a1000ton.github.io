b19172020de665f1723480545906ee337961a7cc
Amilton Vanderlei Baracy Junior <laamilton.a@MacBook-Pro-de-Amilton.local>
2020-10-20 19:01:03 -0300

    Updates

---
+++ component---src-pages-404-js-5a565614f86997a61afc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),o=t("Bl7J"),u=t("vrFN");n.default=function(){return l.a.createElement(o.a,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h1",null,"404: Not Found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-5a565614f86997a61afc.js.map
